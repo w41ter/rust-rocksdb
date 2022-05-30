da9b0b2950f4135d78d1ed6fc89e1008097e2718
w41ter <w41ter.l@gmail.com>
2022-05-30 10:41:01 +0800

    add rocksdb_writebatch_create_from

--- src/write_batch.rs
+++ src/write_batch.rs
@@ -90,6 +90,19 @@ unsafe extern "C" fn writebatch_delete_callback(state: *mut c_void, k: *const c_
 }
 
 impl<const TRANSACTION: bool> WriteBatchWithTransaction<TRANSACTION> {
+    pub fn new(data: &[u8]) -> Self {
+        unsafe {
+            let ptr = data.as_ptr();
+            let len = data.len();
+            Self {
+                inner: ffi::rocksdb_writebatch_create_from(
+                    ptr as *const libc::c_char,
+                    len as size_t,
+                ),
+            }
+        }
+    }
+
     pub fn len(&self) -> usize {
         unsafe { ffi::rocksdb_writebatch_count(self.inner) as usize }
     }
